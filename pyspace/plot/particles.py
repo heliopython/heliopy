eb940e1d9511d0d0ae13ff5a46c55eee6b703b8b
David Stansby <dstansby@gmail.com>
2016-10-26 10:26:02 +0100

    Add 2D contour plot for distribution functions

--- pyspace/plot/particles.py
+++ pyspace/plot/particles.py
@@ -9,3 +9,14 @@ def scatter3D(x, y, z, pdf):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(x, y, z, c=np.log10(pdf), s=40)
+
+
+def contour2D(x, y, pdf, showbins=True):
+    '''
+    Do a countour plot of 2D distribution function data
+    '''
+    fig = plt.figure()
+    ax = plt.subplot()
+    ax.tricontourf(x, y, np.log10(pdf), cmap='viridis', linewidths='1')
+    if showbins:
+        ax.scatter(x, y, c=np.log10(pdf), cmap='viridis', s=40)
