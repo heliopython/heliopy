6760b1aa20e57405694cf775b3963f5251d59f9e
David Stansby <dstansby@gmail.com>
2016-10-26 11:31:44 +0100

    Method to surf 2D distribution function

--- pyspace/plot/particles.py
+++ pyspace/plot/particles.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
@@ -20,3 +21,12 @@ def contour2D(x, y, pdf, showbins=True):
     ax.tricontourf(x, y, np.log10(pdf), cmap='viridis', linewidths='1')
     if showbins:
         ax.scatter(x, y, c=np.log10(pdf), cmap='viridis', s=40)
+
+
+def surf2D(x, y, pdf):
+    '''
+    3D surf plot of 2D distribution function
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(x, y, np.log10(pdf), cmap='viridis', linewidths=0, alpha=0.5)
