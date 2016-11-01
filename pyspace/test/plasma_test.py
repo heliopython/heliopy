0c9c30028858c7c4072d9147bd3e4b887727665c
David Stansby <dstansby@gmail.com>
2016-11-01 14:43:17 +0000

    Add plasma test

---
+++ pyspace/test/plasma_test.py
@@ -0,0 +1,8 @@
+from pyspace.plasma import *
+from pyspace.constants import *
+import numpy as np
+
+
+def test_magneticpressure():
+    assert magneticpressure(0) = 0
+    assert magneticpressure(1e9 * np.sqrt(k_B * 2)) == 1.0
