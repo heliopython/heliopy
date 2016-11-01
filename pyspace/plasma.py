73b4c82adff64a248e1af668b6ee8b00285f6c47
David Stansby <dstansby@gmail.com>
2016-11-01 14:40:06 +0000

    Add method to work out magnetic pressure

---
+++ pyspace/plasma.py
@@ -0,0 +1,16 @@
+'''
+Methods for working out various quanties of interest to plasmas
+'''
+import constants
+
+
+def magneticpressure(B):
+    '''
+    Returns magnetic pressure given magnetic field magnitude
+
+    B must be in nT
+    '''
+    # Convert to Tesla
+    B *= 1e-9
+    p = B**2 / (2 * constants.mu_0)
+    return p
