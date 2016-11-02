7b6489e51688f02cb973caf1032d48baab8f062b
David Stansby <dstansby@gmail.com>
2016-11-02 14:08:57 +0000

    Look for configuration file in home directory before using default configuration

--- pyspace/util/config.py
+++ pyspace/util/config.py
@@ -11,11 +11,17 @@ def load_config():
     Read in configuration file
     '''
     config_filename = 'pyspacerc'
-
     config = configparser.ConfigParser()
-    # Get configuration location
+
+    # Get user configuration location
+    home_dir = os.path.expanduser("~")
+    config_file = os.path.join(home_dir, '.pyspace', config_filename)
+    if os.path.isfile(config_file):
+        config.read(config_file)
+        return config
+
+    # Get default configuration location
     module_dir = os.path.dirname(pyspace.__file__)
     config_file = os.path.join(module_dir, 'data', config_filename)
-    # Read and return configuration
     config.read(config_file)
     return config
