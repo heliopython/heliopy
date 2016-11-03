ec4a28328c9973472a9c2b095352715ed01125cd
David Stansby <dstansby@gmail.com>
2016-11-03 13:12:11 +0000

    Don't make new figures in plotting methods

--- heliopy/plot/particles.py
+++ heliopy/plot/particles.py
@@ -7,7 +7,7 @@ def scatter3D(x, y, z, pdf):
     '''
     Perform 3D scatter plot of distribution function data
     '''
-    fig = plt.figure()
+    fig = plt.gcf()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(x, y, z, c=np.log10(pdf), s=40)
 
@@ -16,7 +16,6 @@ def contour2D(x, y, pdf, showbins=True):
     '''
     Do a countour plot of 2D distribution function data
     '''
-    fig = plt.figure()
     ax = plt.subplot()
     ax.tricontourf(x, y, np.log10(pdf), cmap='viridis', linewidths='1')
     if showbins:
@@ -27,6 +26,5 @@ def surf2D(x, y, pdf):
     '''
     3D surf plot of 2D distribution function
     '''
-    fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_trisurf(x, y, np.log10(pdf), cmap='viridis', linewidths=0, alpha=0.5)
