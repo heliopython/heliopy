3ccee098cf940500e7027eb336638815aab457d9
David Stansby <dstansby@gmail.com>
2016-11-11 22:02:37 +0000

    Add docstring to histogram method

--- heliopy/stats.py
+++ heliopy/stats.py
@@ -1,12 +1,31 @@
-"""
-Statistical methods
-"""
+"""Statistical methods"""
 import numpy as np
 
 
 def hist(x, bins='auto', normed=True, return_centres=True):
     """
     Improved histogram function
+
+    Parameters
+    ----------
+        x : array_like
+            Data values.
+        bins : int | string
+            Number of bins in output.
+        normed : bool
+            If true, return a normalised histogram such that the sum of values
+            in each bin is 1.
+        return_centres : bool
+            If True, returns the co-ordinates of the bin centres. If False,
+            returns the co-ordinates of the bin edges.
+
+    Returns
+    -------
+        hist : array_like
+            Histogram values.
+        bins : array_like
+            Bin centres or bin edges (depends on value of return_centres
+            argument)
     """
     hist, bin_edges = np.histogram(x, bins=bins, normed=normed)
     # Calculate centres of bins (e.g. for plotting scatter pdf)
