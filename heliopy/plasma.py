db10bcc8e8499ed712aa108886e0a65d9389e033
David Stansby <dstansby@gmail.com>
2016-11-15 15:11:37 +0000

    Method to convert thermal velocity to temperature!

--- heliopy/plasma.py
+++ heliopy/plasma.py
@@ -4,6 +4,27 @@ Methods for working out various quanties of interest to plasmas
 from heliopy import constants
 
 
+def vth2temp(v, m):
+    """
+    Converts thermal velocity to temperature.
+
+    Parameters
+    ----------
+        v : array_like
+            Thermal velocities in km/s.
+        m : float
+            Particle mass in kg.
+
+    Returns
+    -------
+        t : array_like
+            Temperatures.
+    """
+    v *= 1e3
+    t = m * v**2 / constants.k_B
+    return t
+
+
 def magneticpressure(B):
     """
     Returns magnetic pressure given magnetic field magnitude.
