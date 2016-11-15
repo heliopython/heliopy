a8ba292a82fd5adbf0e73e7fca5dbcf9036e827d
David Stansby <dstansby@gmail.com>
2016-11-15 17:41:24 +0000

    Add tests for plasma library

--- heliopy/test/plasma_test.py
+++ heliopy/test/plasma_test.py
@@ -1,6 +1,23 @@
 from heliopy.plasma import *
+import heliopy.constants as const
 import numpy as np
 
 
 def test_magneticpressure():
     assert magneticpressure(0) == 0
+
+    out = magneticpressure(1e9)
+    expected = 1 / (2 * const.mu_0)
+    np.testing.assert_almost_equal(out, expected)
+
+
+def test_vth2temp():
+    out = vth2temp(1e-3, 1)
+    expected = 1 / const.k_B
+    np.testing.assert_almost_equal(out, expected)
+
+
+def test_alfvenspeed():
+    out = alfvenspeed(1e-6, 1e9)
+    expected = 1e-3 / np.sqrt(const.m_p * const.mu_0)
+    np.testing.assert_almost_equal(out, expected)
