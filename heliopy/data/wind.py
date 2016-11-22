4051048e9a01c6f838b1b093f4355919b49136cb
David Stansby <dstansby@gmail.com>
2016-11-22 17:38:54 +0000

    Add method to import wind magnetic field data

--- heliopy/data/wind.py
+++ heliopy/data/wind.py
@@ -11,6 +11,52 @@ wind_dir = data_dir + '/wind'
 remote_wind_dir = 'ftp://spdf.gsfc.nasa.gov/pub/data/wind'
 
 
+def mfi_h0(starttime, endtime):
+    """
+    Import 'mfi_h0' magnetic field data product from WIND.
+
+    Parameters
+    ----------
+        starttime : datetime
+            Interval start time.
+        endtime : datetime
+            Interval end time.
+
+    Returns
+    -------
+        data : DataFrame
+    """
+    # Directory relative to main WIND data directory
+    relative_dir = '/mfi/mfi_h0'
+
+    daylist = spacetime.daysplitinterval(starttime, endtime)
+    data = []
+    for day in daylist:
+        date = day[0]
+        filename = 'wi_h0_mfi_' +\
+            str(date.year) +\
+            str(date.month).zfill(2) +\
+            str(date.day).zfill(2) +\
+            '_v05.cdf'
+        this_relative_dir = relative_dir + '/' + str(day[0].year)
+        # Absolute path to local directory for this data file
+        local_dir = wind_dir + this_relative_dir
+        helper.checkdir(local_dir)
+
+        remote_url = remote_wind_dir + this_relative_dir
+
+        cdf = helper.load(filename, local_dir, remote_url)
+
+        keys = {'B3GSE': ['Bx_gse', 'By_gse', 'Bz_gse'],
+                'Epoch3': 'Time'}
+        df = helper.cdf2df(cdf, index_key='Epoch3', keys=keys)
+        data.append(df)
+
+    data = pd.concat(data)
+    data = data[(data['Time'] > starttime) & (data['Time'] < endtime)]
+    return data
+
+
 def threedp_pm(starttime, endtime):
     """
     Import 'pm' wind data.
