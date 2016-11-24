7cecd16f8be69eda8bc7ebf12f78fa88b03d4a61
David Stansby <dstansby@gmail.com>
2016-11-24 17:01:23 +0000

    Test all wind importing

--- heliopy/data/test/data_test.py
+++ heliopy/data/test/data_test.py
@@ -1,8 +1,16 @@
 import heliopy.data.wind as wind
 from datetime import datetime
+import urllib
 
 
-def test_mfi_h0():
+def test_wind():
+    """
+    Tests for imporitng wind data.
+
+    Currently only try to download a single day's data for each product.
+    """
     starttime = datetime(2010, 1, 1, 0, 0, 0)
     endtime = datetime(2010, 1, 1, 23, 59, 59)
+
     wind.mfi_h0(starttime, endtime)
+    wind.threedp_pm(starttime, endtime)
