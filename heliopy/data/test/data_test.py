8d1fa26c5e7617be437c0523fe3d0eaa69921da7
David Stansby <dstansby@gmail.com>
2016-11-26 12:15:41 +0000

    Add mms tests

--- heliopy/data/test/data_test.py
+++ heliopy/data/test/data_test.py
@@ -1,4 +1,5 @@
 import heliopy.data.wind as wind
+import heliopy.data.mms as mms
 from datetime import datetime
 import urllib
 
@@ -14,3 +15,19 @@ def test_wind():
 
     wind.mfi_h0(starttime, endtime)
     wind.threedp_pm(starttime, endtime)
+
+
+def test_mms():
+    """
+    Tests for importing mms data.
+
+    Try and import a single days' worth of data for each data product.
+    """
+    starttime = datetime(2016, 1, 1, 0, 0, 0)
+    endtime = datetime(2016, 1, 1, 1, 0, 0)
+
+    for i in range(1, 5):
+        mms.fgm_survey(str(i), starttime, endtime)
+
+if __name__ == '__main__':
+    test_mms()
