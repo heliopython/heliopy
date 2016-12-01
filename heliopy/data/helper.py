3f1ebef6f3132e98d73ec031723a814f27bf9a7c
David Stansby <dstansby@gmail.com>
2016-12-01 17:41:08 +0000

    Make reporthook public

--- heliopy/data/helper.py
+++ heliopy/data/helper.py
@@ -7,7 +7,7 @@ import ftplib
 import pandas as pd
 
 
-def _reporthook(blocknum, blocksize, totalsize):
+def reporthook(blocknum, blocksize, totalsize):
     readsofar = blocknum * blocksize
     if totalsize > 0:
         percent = readsofar * 1e2 / totalsize
@@ -56,7 +56,7 @@ def _load_remote(remote_url, filename, local_dir, filetype):
         print('Downloading', remote_url + '/' + filename)
         urlretrieve(remote_url + '/' + filename,
                     filename=local_dir + '/' + filename,
-                    reporthook=_reporthook)
+                    reporthook=reporthook)
         return _load_local(local_dir, filename, filetype)
 
 
