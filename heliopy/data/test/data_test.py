bf5054148cfbff8f754069e1d1305afd5ec1224c
David Stansby <dstansby@gmail.com>
2016-12-02 15:07:12 +0000

    Add cluster data test

--- heliopy/data/test/data_test.py
+++ heliopy/data/test/data_test.py
@@ -1,8 +1,13 @@
 import heliopy.data.wind as wind
 import heliopy.data.mms as mms
 import heliopy.data.helios as helios
+import heliopy.data.cluster as cluster
 from datetime import datetime
 import urllib
+
+
+def test_cluster():
+    cluster.fgm('2', datetime(2004, 6, 18, 11, 35, 0), datetime(2004, 6, 19, 18, 35, 0))
 
 
 def test_wind():
