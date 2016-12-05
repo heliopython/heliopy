5ec94e46666d5217825fc2ea770951f423796534
David Stansby <dstansby@gmail.com>
2016-12-05 13:47:32 +0000

    Fix solar rotation test

--- heliopy/bodies/test/bodies_test.py
+++ heliopy/bodies/test/bodies_test.py
@@ -5,5 +5,5 @@ import heliopy.bodies.sun as sun
 
 def test_rotationrate():
     lat_rand = np.random.rand() * np.pi / 2
-    assert rotationrate(lat_rand) < rotationrate(0)
-    assert rotationrate(lat_rand) == rotationrate(-lat_rand)
+    assert sun.rotationrate(lat_rand) < sun.rotationrate(0)
+    assert sun.rotationrate(lat_rand) == sun.rotationrate(-lat_rand)
