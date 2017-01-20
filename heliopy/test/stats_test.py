12c194b8b25c6d7f082a4910e6ee5004f2809754
David Stansby <dstansby@gmail.com>
2017-01-20 11:41:40 +0000

    Use density kwarg instead of broken normed kwarg in hist

--- heliopy/test/stats_test.py
+++ heliopy/test/stats_test.py
@@ -6,7 +6,7 @@ def test_hist():
     x = np.array([1, 2, 2, 3])
     out, bins = hist(x, bins=3, return_centres=True)
 
-    np.testing.assert_equal(out, np.array([0.375, 0.75, 0.375]))
+    np.testing.assert_almost_equal(out, np.array([0.375, 0.75, 0.375]))
     np.testing.assert_almost_equal(bins, np.array([4 / 3, 6 / 3, 8 / 3]))
 
 
