fa9b7341e529f569e64fd9a2faaf0674dcb96df3
David Stansby <dstansby@gmail.com>
2017-01-20 12:09:43 +0000

    Clean up loglog plotting and add example

--- heliopy/plot/spectra.py
+++ heliopy/plot/spectra.py
@@ -2,12 +2,13 @@
 import matplotlib.pyplot as plt
 
 
-def loglog(fs, power, title=None, xlabel=r'$f /Hz$', ylabel='', legend=None,
+def loglog(fs, power, title=None, xlabel=None, ylabel=None, legend=None,
            ax=None):
     """
     Plot power spectra on log-log axes.
 
-    Gets current axis and adds plot. Can also label plot.
+    Plotting is done on the current axis if present, otherwise a new axis will
+    be created. Alternatively `ax` can be used to specify the axis.
 
     Parameters
     ----------
@@ -25,6 +26,13 @@ def loglog(fs, power, title=None, xlabel=r'$f /Hz$', ylabel='', legend=None,
             Legend entry
         ax : matplotlib axis instance
             Axis to plot on.
+
+    Examples
+    --------
+    A simple log-log plot:
+
+    .. literalinclude:: /examples/scripts/plot_loglog.py
+    .. image:: /figures/plot_loglog.png
     """
     if ax is None:
         ax = plt.gca()
@@ -32,10 +40,12 @@ def loglog(fs, power, title=None, xlabel=r'$f /Hz$', ylabel='', legend=None,
 
     # Figure formatting
     if title is not None:
-        plt.title(title)
+        ax.title(title)
     if legend is not None:
-        plt.legend()
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+        ax.legend()
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
     ax.set_xscale('log')
     ax.set_yscale('log')
