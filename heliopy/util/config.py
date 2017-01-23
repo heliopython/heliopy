0d4d1cef664bfd3735d1727efd00184752f9c7b0
David Stansby <dstansby@gmail.com>
2017-01-23 18:03:16 +0000

    Add option to save to hdf files

--- heliopy/util/config.py
+++ heliopy/util/config.py
@@ -14,14 +14,15 @@ def load_config():
     config_file = os.path.join(home_dir, '.heliopy', config_filename)
     if os.path.isfile(config_file):
         config.read(config_file)
-        return config
+    else:
+        module_dir = os.path.dirname(heliopy.__file__)
+        config_file = os.path.join(module_dir, config_filename)
+        if os.path.isfile(config_file):
+            config.read(config_file)
 
-    # Get default configuration location
-    module_dir = os.path.dirname(heliopy.__file__)
-    config_file = os.path.join(module_dir, 'data', config_filename)
-    config.read(config_file)
     config['default']['download_dir'] = os.path.join(home_dir,
                                                      config['default']['download_dir'])
+    # Check environment variables for a cluster cookie
     if os.environ.get('CLUSTERCOOKIE') is not None and\
             config['default']['download_dir'] == 'none':
         config['cluster']['user_cookie'] = os.environ.get('CLUSTERCOOKIE')
