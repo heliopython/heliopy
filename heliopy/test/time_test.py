18b8f6437ad21ba41b6a65fe1a82eb8eb2b20e41
David Stansby <dstansby@gmail.com>
2017-01-23 11:44:53 +0000

    Simplify doy2ymd method

--- heliopy/test/time_test.py
+++ heliopy/test/time_test.py
@@ -31,7 +31,3 @@ def test_daysplitinterval():
 def test_doy2ymd():
     assert doy2ymd(2015, 3) == (2015, 1, 3)
     assert doy2ymd(2016, 32) == (2016, 2, 1)
-    with pytest.raises(ValueError):
-        doy2ymd(2014, 366)
-    with pytest.raises(ValueError):
-        doy2ymd(2012, 367)
