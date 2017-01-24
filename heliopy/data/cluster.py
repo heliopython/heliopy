cb9931cebc61c02ef42d019fb0a4ed9ab0c6fa93
David Stansby <dstansby@gmail.com>
2017-01-24 12:55:51 +0000

    Correctly set cluster cookie

--- heliopy/data/cluster.py
+++ heliopy/data/cluster.py
@@ -22,7 +22,7 @@ from heliopy.data.helper import reporthook, checkdir, cdf2df, timefilter
 from spacepy import pycdf
 
 data_dir = config['DEFAULT']['download_dir']
-cda_cookie = config['DEFAULT']['user_cookie']
+cda_cookie = config['DEFAULT']['cluster_cookie']
 csa_url = 'https://csa.esac.esa.int/csa/aio/product-action?'
 cluster_dir = os.path.join(data_dir, 'cluster')
 
