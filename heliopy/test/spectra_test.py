fbccde06188c38d808153f37aca9389097edc4d0
David Stansby <dstansby@gmail.com>
2017-03-03 16:34:40 +0000

    Fix spectral slope test

--- heliopy/test/spectra_test.py
+++ heliopy/test/spectra_test.py
@@ -5,6 +5,6 @@ import numpy as np
 def test_spectral_slopes():
     x = np.array([1, 2, 3, 4])
     y = x**-0.5
-    out = spectral_slopes(x, y, nbins=1)
+    out = spectral_slopes(x, y, bins=1)
     np.testing.assert_equal(out[0], [1, 4])
     np.testing.assert_almost_equal(out[1], [-0.5])
