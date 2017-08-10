0fc963b7e4859c7d64b29d016157d1e77aee9c9e
David Stansby <dstansby@gmail.com>
2017-08-10 17:58:09 +0100

    ...

--- ci/install_conda.sh
+++ ci/install_conda.sh
@@ -5,5 +5,5 @@ then
   wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   "./miniconda.sh -b -f -p $HOME/miniconda"
-  export PATH="$HOME/miniconda/bin:$PATH"
 fi
+export PATH="$HOME/miniconda/bin:$PATH"
