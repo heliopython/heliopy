7e6d908c1cd253e94edb6ec4c8703a64d11d7808
David Stansby <dstansby@gmail.com>
2017-08-23 11:40:35 +0100

    Remove timedelta2ordinal method

--- heliopy/time.py
+++ heliopy/time.py
@@ -4,28 +4,6 @@ import numpy as np
 import pandas as pd
 
 
-def dtime2ordinal(dtime):
-    """
-    Converts datetime to ordinal.
-
-    Particularly useful for interpolating different time series on to each
-    other.
-
-    Parameters
-    ----------
-    dtime : array_like or datetime
-        Input datetime array or single datetime value
-
-    Returns
-    -------
-    ordinal : array_like
-        Converted datetimes as ordinals
-    """
-    if type(dtime) == datetime:
-        dtime = pd.Series(dtime)
-    return pd.DatetimeIndex(dtime).astype(np.int64)
-
-
 def nptimedelta2seconds(dt):
     """
     Converts a numpy timedelta array into seconds.
