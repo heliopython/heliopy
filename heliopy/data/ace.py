65a177568069d8a7e654f5d07f3542e15a13dd26
David Stansby <dstansby@gmail.com>
2017-09-06 21:52:26 +0100

    Add method to import ACE swe data (proton moments)

--- heliopy/data/ace.py
+++ heliopy/data/ace.py
@@ -14,6 +14,7 @@ from heliopy import config
 data_dir = config['DEFAULT']['download_dir']
 ace_dir = os.path.join(data_dir, 'ace')
 remote_ace_dir = 'ftp://spdf.gsfc.nasa.gov/pub/data/ace/'
+remote_cda_dir = 'ftp://cdaweb.gsfc.nasa.gov/pub/data/ace/'
 
 
 def mfi_h0(starttime, endtime):
@@ -55,6 +56,64 @@ def mfi_h0(starttime, endtime):
         keys = {'BGSEc': ['Bx_gse', 'By_gse', 'Bz_gse'],
                 'Magnitude': '|B|',
                 'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
+                'Epoch': 'Time'}
+        badvalues = {}
+        df = helper.cdf2df(cdf,
+                           index_key='Epoch',
+                           keys=keys,
+                           badvalues=badvalues)
+        data.append(df)
+
+    data = helper.timefilter(data, starttime, endtime)
+    return data
+
+
+def swe_h0(starttime, endtime):
+    """
+    Import swe_h0 particle moment data product from ACE. See
+    https://cdaweb.sci.gsfc.nasa.gov/misc/NotesA.html#AC_H0_SWE
+    for more information.
+
+    Parameters
+    ----------
+    starttime : datetime
+        Interval start time.
+    endtime : datetime
+        Interval end time.
+
+    Returns
+    -------
+    data : DataFrame
+    """
+    # Directory relative to main WIND data directory
+    relative_dir = os.path.join('swepam', 'level_2_cdaweb', 'swe_h0')
+
+    daylist = spacetime.daysplitinterval(starttime, endtime)
+    data = []
+    for day in daylist:
+        date = day[0]
+        filename = 'ac_h0_swe_' +\
+            str(date.year) +\
+            str(date.month).zfill(2) +\
+            str(date.day).zfill(2) +\
+            '_v06.cdf'
+        this_relative_dir = os.path.join(relative_dir, str(day[0].year))
+        # Absolute path to local directory for this data file
+        local_dir = os.path.join(ace_dir, this_relative_dir)
+        helper.checkdir(local_dir)
+
+        remote_url = remote_cda_dir + this_relative_dir
+        cdf = helper.load(filename, local_dir, remote_url, guessversion=True)
+
+        keys = {'Np': 'n_p',
+                'Tpr': 'T_pr',
+                'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
+                'SC_pos_GSM': ['sc_gsm_x', 'sc_gsm_y', 'sc_gsm_z'],
+                'V_GSE': ['vp_gse_x', 'vp_gse_y', 'vp_gse_z'],
+                'V_GSM': ['vp_gsm_x', 'vp_gsm_y', 'vp_gsm_z'],
+                'V_RTN': ['vp_gse_r', 'vp_gse_t', 'vp_gse_n'],
+                'Vp': '|vp|',
+                'alpha_ratio': 'n_a/n_p',
                 'Epoch': 'Time'}
         badvalues = {}
         df = helper.cdf2df(cdf,
