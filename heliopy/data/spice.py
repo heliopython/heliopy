fe425cb55bfc1ca9a056834629e3e119be898505
David Stansby <dstansby@gmail.com>
2018-01-12 11:46:11 +0000

    Update spice code

---
+++ heliopy/data/spice.py
@@ -0,0 +1,51 @@
+"""
+SPICE
+-----
+
+Methods for automatically downloading SPICE kernels for various objects.
+
+This is essentially a library of SPICE kernels that are available online, so
+users don't have go go hunting for them. If you know a kernel is out of date,
+and HelioPy should be using a newer kernel please let us know at
+https://github.com/heliopython/heliopy/issues.
+"""
+import os
+from urllib.request import urlretrieve
+
+from heliopy import config
+import heliopy.data.helper as helper
+
+data_dir = config['download_dir']
+spice_dir = os.path.join(data_dir, 'spice')
+# Mapping of kernel name to remote location
+available_kernels = {'lsk': 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls',
+                     'planets': 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de432s.bsp'}
+
+
+def get_kernel(name):
+    """
+    Get the local location of a kernel.
+
+    If a kernel isn't available locally, it is download.
+
+    Parameters
+    ----------
+    name : str
+        Kernel name. See PUT LINK HERE for a list of available names.
+
+    Returns
+    -------
+    str
+        Local location of kernel.
+    """
+    if name not in available_kernels:
+        raise ValueError(
+            'Provided name {} not in list of supported names: {}'.format(
+                name, available_kernels))
+    url = available_kernels[name]
+    fname = url.split('/')[-1]
+    local_loc = os.path.join(spice_dir, fname)
+    if not os.path.exists(local_loc):
+        print('Downloading {}'.format(url))
+        urlretrieve(url, local_loc, reporthook=helper._reporthook)
+    return local_loc
--- heliopy/spice/__init__.py
+++ heliopy/spice/__init__.py
@@ -1,16 +1,21 @@
 """
-A module for loading spice kernels.
+SPICE
+-----
+
+A module for loading SPICE kernels.
 
 TODO: improve this docstring.
 """
 from heliopy import config
 import heliopy.data.helper as helper
+import heliopy.data.spice as dataspice
 
 import os
 from urllib.request import urlretrieve
 
 import numpy as np
 import spiceypy as spice
+import astropy.units as u
 
 data_dir = config['download_dir']
 spice_dir = os.path.join(data_dir, 'spice')
@@ -25,20 +30,9 @@ def _setup_spice():
     Method to download some common files that spice needs to do orbit
     calculations.
     '''
-    general_dir = os.path.join(spice_dir, 'generic')
-    if not os.path.exists(general_dir):
-        print('Creating {} directory'.format(general_dir))
-        os.makedirs(general_dir)
-
-    local_reqs = []
-    for req in _reqs:
-        fname = req.split('/')[-1]
-        local_loc = os.path.join(general_dir, fname)
-        if not os.path.exists(local_loc):
-            print('Downloading {}'.format(req))
-            urlretrieve(req, local_loc, reporthook=helper._reporthook)
-            local_reqs.append(local_loc)
-    return local_reqs
+    for kernel in ['lsk', 'planets']:
+        loc = dataspice.get_kernel(kernel)
+        spice.furnsh(loc)
 
 
 class SpiceKernel:
@@ -56,22 +50,23 @@ class SpiceKernel:
     Attributes
     ----------
     generated : bool
+        ``True`` if positions have been generated, ``False`` otherwise.
     times : list
+        The list of `datetime`s at which positions were last sampled.
     n : int
+        The number of samples.
     positions :
+        Generated positions.
+    observing_body : str
+        Observing body
     """
     def __init__(self, target, fname=None):
-        if fname is None and spacecraft is None:
-            raise ValueError('Either a filename or a spacecraft must be '
-                             'provided.')
         local_reqs = _setup_spice()
-        for req in local_reqs:
-            spice.furnsh(req)
         spice.furnsh(fname)
         self.target = target
         self.generated = False
 
-    def generate_positions(self, starttime, endtime, n, observing_body):
+    def generate_positions(self, starttime, endtime, n, observing_body, frame):
         """
         Generate positions from a spice kernel.
 
@@ -86,27 +81,26 @@ class SpiceKernel:
         observing_body : str
             The observing body. See https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/naif_ids.html
             for a list of allowable bodies.
+        frame : str
+            The coordinate system to return the positions in.
+            See https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/frames.html
+            for a list of allowable frames.
         """
-        # TODO: Make this accept the input times
-        utc = ['Mar 1, 2020', 'Dec 31, 2028']
-        etOne = spice.str2et(utc[0])
-        etTwo = spice.str2et(utc[1])
+        dt = (endtime - starttime)
         # Generate individual times within range
+        times = [starttime + (x * dt / n) for x in range(n)]
+        # Spice needs a funny set of times
+        fmt = '%b %d, %Y'
+        etOne = spice.str2et(starttime.strftime(fmt))
+        etTwo = spice.str2et(endtime.strftime(fmt))
         spice_times = [x * (etTwo - etOne) / n + etOne for x in range(n)]
         observing_body = observing_body
-        # TODO: Make this an argument
-        frame = 'ECLIPJ2000'
         # TODO: Work out what "NONE" does here
         positions, lightTimes = spice.spkpos(
-            self.target, spice_times, 'ECLIPJ2000', 'NONE', observing_body)
-        positions = np.array(positions)
-        # TODO: Work out how to return these using units or coordinates
-        # Positions is a nx3 vector of XYZ positions
-        # Convert from km to AU
-        positions = positions / 1.496e8
+            self.target, spice_times, frame, 'NONE', observing_body)
+        positions = np.array(positions) * u.km
 
         self.n = n
-        # TODO: Make this a list of datetimes or similar
-        self.times = spice_times
+        self.times = times
         self.positions = positions
         self.generated = True
