a6558dcdba12298ac587190c3265e4e507a70d6e
David Stansby <dstansby@gmail.com>
2018-01-16 13:28:17 +0000

    Update mulit-index example for sphinx gallery

--- examples/plot_multi_index.py
+++ examples/plot_multi_index.py
@@ -40,15 +40,6 @@ index = pd.MultiIndex.from_product((E, theta, phi),
                                    names=('E', 'theta', 'phi'))
 data = pd.DataFrame(data=dist, index=index, columns=['df'])
 print(data.head())
-"""
-                   df
-E theta phi
-0 -90   0    0.369417
-        90   0.074464
-        180  0.470669
-        270  0.320014
-  -45   0    0.607422
-"""
 
 ###############################################################################
 # To loop through the energy levels with the index label ``'theta'`` do the
@@ -57,9 +48,3 @@ E theta phi
 
 for theta, theta_data in data.groupby(level='theta'):
     print(theta)
-"""
--90
--45
-0
-45
-"""
