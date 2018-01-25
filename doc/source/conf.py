8cc068f3e7301c03ac016e35abb575127cf9aca1
David Stansby <dstansby@gmail.com>
2018-01-25 17:51:41 +0000

    Add spiceypy to doc requirements

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 html_sidebars = {'**': ['docsidebar.html']}
 
 # Pretend these modules exits so readthedocs builds
-MOCK_MODULES = ['spiceypy']
+MOCK_MODULES = []
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
