f23b3cccbf223f5d7eff5d52fab7692a2d77eceb
David Stansby <dstansby@gmail.com>
2018-02-07 13:40:43 +0000

    Make wrapper function for downloading ACE data

--- heliopy/data/ace.py
+++ heliopy/data/ace.py
@@ -16,6 +16,36 @@ remote_ace_dir = 'ftp://spdf.gsfc.nasa.gov/pub/data/ace/'
 remote_cda_dir = 'ftp://cdaweb.gsfc.nasa.gov/pub/data/ace/'
 
 
+def _ace(starttime, endtime, instrument, product, fname, keys, version='01'):
+    """
+    Generic method for downloading ACE data from cdaweb.
+    """
+    # Directory relative to main WIND data directory
+    relative_dir = os.path.join(instrument, 'level_2_cdaweb', product)
+
+    daylist = helper._daysplitinterval(starttime, endtime)
+    data = []
+    for day in daylist:
+        date = day[0]
+        filename = 'ac_{}_{}{:02}{:02}_v{}.cdf'.format(
+            fname, date.year, date.month, date.day, version)
+        this_relative_dir = os.path.join(relative_dir, str(date.year))
+        # Absolute path to local directory for this data file
+        local_dir = os.path.join(ace_dir, this_relative_dir)
+        helper._checkdir(local_dir)
+
+        remote_url = remote_ace_dir + this_relative_dir
+        cdf = helper.load(filename, local_dir, remote_url, guessversion=True)
+
+        badvalues = {}
+        df = helper.cdf2df(cdf,
+                           index_key='Epoch',
+                           keys=keys)
+        data.append(df)
+
+    data = helper.timefilter(data, starttime, endtime)
+    return data
+
 def mfi_h0(starttime, endtime):
     """
     Import 'mfi_h0' magnetic field data product from ACE. See
@@ -32,36 +62,15 @@ def mfi_h0(starttime, endtime):
     -------
     data : DataFrame
     """
-    # Directory relative to main WIND data directory
-    relative_dir = os.path.join('mag', 'level_2_cdaweb', 'mfi_h0')
-
-    daylist = helper._daysplitinterval(starttime, endtime)
-    data = []
-    for day in daylist:
-        date = day[0]
-        filename = 'ac_h0_mfi_{}{:02}{:02}_v06.cdf'.format(
-            date.year, date.month, date.day)
-        this_relative_dir = os.path.join(relative_dir, str(date.year))
-        # Absolute path to local directory for this data file
-        local_dir = os.path.join(ace_dir, this_relative_dir)
-        helper._checkdir(local_dir)
-
-        remote_url = remote_ace_dir + this_relative_dir
-        cdf = helper.load(filename, local_dir, remote_url, guessversion=True)
-
-        keys = {'BGSEc': ['Bx_gse', 'By_gse', 'Bz_gse'],
-                'Magnitude': '|B|',
-                'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
-                'Epoch': 'Time'}
-        badvalues = {}
-        df = helper.cdf2df(cdf,
-                           index_key='Epoch',
-                           keys=keys,
-                           badvalues=badvalues)
-        data.append(df)
-
-    data = helper.timefilter(data, starttime, endtime)
-    return data
+    instrument = 'mag'
+    product = 'mfi_h0'
+    fname = 'h0_mfi'
+    keys = {'BGSEc': ['Bx_gse', 'By_gse', 'Bz_gse'],
+            'Magnitude': '|B|',
+            'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
+            'Epoch': 'Time'}
+    version = '06'
+    return _ace(starttime, endtime, instrument, product, fname, keys, version)
 
 
 def swe_h0(starttime, endtime):
@@ -81,42 +90,18 @@ def swe_h0(starttime, endtime):
     -------
     data : DataFrame
     """
-    # Directory relative to main WIND data directory
-    relative_dir = os.path.join('swepam', 'level_2_cdaweb', 'swe_h0')
-
-    daylist = helper._daysplitinterval(starttime, endtime)
-    data = []
-    for day in daylist:
-        date = day[0]
-        filename = 'ac_h0_swe_' +\
-            str(date.year) +\
-            str(date.month).zfill(2) +\
-            str(date.day).zfill(2) +\
-            '_v06.cdf'
-        this_relative_dir = os.path.join(relative_dir, str(day[0].year))
-        # Absolute path to local directory for this data file
-        local_dir = os.path.join(ace_dir, this_relative_dir)
-        helper._checkdir(local_dir)
-
-        remote_url = remote_cda_dir + this_relative_dir
-        cdf = helper.load(filename, local_dir, remote_url, guessversion=True)
-
-        keys = {'Np': 'n_p',
-                'Tpr': 'T_pr',
-                'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
-                'SC_pos_GSM': ['sc_gsm_x', 'sc_gsm_y', 'sc_gsm_z'],
-                'V_GSE': ['vp_gse_x', 'vp_gse_y', 'vp_gse_z'],
-                'V_GSM': ['vp_gsm_x', 'vp_gsm_y', 'vp_gsm_z'],
-                'V_RTN': ['vp_rtn_r', 'vp_rtn_t', 'vp_rtn_n'],
-                'Vp': '|vp|',
-                'alpha_ratio': 'n_a/n_p',
-                'Epoch': 'Time'}
-        badvalues = {}
-        df = helper.cdf2df(cdf,
-                           index_key='Epoch',
-                           keys=keys,
-                           badvalues=badvalues)
-        data.append(df)
-
-    data = helper.timefilter(data, starttime, endtime)
-    return data
+    instrument = 'swepam'
+    product = 'swe_h0'
+    fname = 'h0_swe'
+    keys = {'Np': 'n_p',
+            'Tpr': 'T_pr',
+            'SC_pos_GSE': ['sc_gse_x', 'sc_gse_y', 'sc_gse_z'],
+            'SC_pos_GSM': ['sc_gsm_x', 'sc_gsm_y', 'sc_gsm_z'],
+            'V_GSE': ['vp_gse_x', 'vp_gse_y', 'vp_gse_z'],
+            'V_GSM': ['vp_gsm_x', 'vp_gsm_y', 'vp_gsm_z'],
+            'V_RTN': ['vp_rtn_r', 'vp_rtn_t', 'vp_rtn_n'],
+            'Vp': '|vp|',
+            'alpha_ratio': 'n_a/n_p',
+            'Epoch': 'Time'}
+    version = '06'
+    return _ace(starttime, endtime, instrument, product, fname, keys, version)
