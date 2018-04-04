e7f57cacda713cd0a4ec4748b88861706341ee2b
Yatharth <36379971+abotiamnot@users.noreply.github.com>
2018-04-04 19:07:18 +0530

    Update test_data.py

--- heliopy/data/test/test_data.py
+++ heliopy/data/test/test_data.py
@@ -165,7 +165,7 @@ class TestImp:
         check_datetime_index(df)
 
     def test_mag15s(self):
-        df = imp.mag15s(probe, self.starttime, self.endtime)
+        df = imp.mag15s(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
 
     def test_mitplasma_h0(self):
