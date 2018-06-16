5aa57fe85ce2762eca34a515adba96bf342081b6
abotiamnot <yatharthrai16@ducic.ac.in>
2018-06-16 12:50:39 +0530

    Added more extensive testing for units

--- heliopy/data/test/test_data.py
+++ heliopy/data/test/test_data.py
@@ -53,12 +53,18 @@ class TestCassini:
     def test_mag(self):
         df = cassini.mag_hires(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("cassini.mag_hires returning units")
+            check_units(df)
 
         # Check that a RTN co-ordinate download works too
         starttime = datetime(2004, 5, 1)
         endtime = datetime(2004, 5, 2)
         df = cassini.mag_hires(starttime, endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("cassini.mag_hires returning units")
+            check_units(df)
 
         # Check that no data raises an error
         starttime = datetime(2040, 5, 1)
@@ -68,7 +74,9 @@ class TestCassini:
 
         df = cassini.mag_1min(self.starttime, self.endtime, 'KSO')
         check_datetime_index(df.data)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("cassini.mag_hires returning units")
+            check_units(df)
 
 
 @pytest.mark.skipif(no_pycdf, reason='Importing pycdf failed')
@@ -82,6 +90,9 @@ class TestMessenger:
     def test_mag(self):
         df = messenger.mag_rtn(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("messenger.mag_rtn returning units")
+            check_units(df)
 
 
 @pytest.mark.data
@@ -94,25 +105,34 @@ class TestUlysses:
     def test_fgm_hires(self):
         df = ulysses.fgm_hires(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ulysses.fgm_hires returning units")
+            check_units(df)
 
     def test_swoops_ions(self):
         df = ulysses.swoops_ions(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ulysses.swoops_ions returning units")
+            check_units(df)
 
     def test_swics_heavy_ions(self):
         df = ulysses.swics_heavy_ions(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ulysses.swics_heavy_ions returning units")
+            check_units(df)
 
     def test_swics_abundances(self):
         df = ulysses.swics_abundances(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ulysses.swics_abundances returning units")
+            check_units(df)
 
 
 @pytest.mark.skipif(no_pycdf, reason='Importing pycdf failed')
-@pytest.mark.data
+@pytest.mark.data       
 class TestArtemis:
     @classmethod
     def setup_class(self):
@@ -123,6 +143,9 @@ class TestArtemis:
     def test_fgm(self):
         df = artemis.fgm(self.probe, 'l', 'dsl', self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("artemis.fgm returning units")
+            check_units(df)
 
         with pytest.raises(ValueError):
             artemis.fgm('123', 'h', 'dsl', self.starttime, self.endtime)
@@ -143,27 +166,37 @@ class TestAce:
     def test_mfi_h0(self):
         df = ace.mfi_h0(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ace.mfi_h0 returning units")
+            check_units(df)
 
     def test_swe_h0(self):
         df = ace.swe_h0(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ace.swe_h0 returning units")
+            check_units(df)
 
     def test_swi_h2(self):
         df = ace.swi_h2(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ace.swi_h2 returning units")
+            check_units(df)
 
     def test_swi_h3(self):
         df = ace.swi_h3(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ace.swi_h3 returning units")
+            check_units(df)
 
     def test_swi_h6(self):
         df = ace.swi_h6(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("ace.swi_h6 returning units")
+            check_units(df)
 
 
 @pytest.mark.skipif(no_pycdf, reason='Importing pycdf failed')
@@ -178,18 +211,30 @@ class TestImp:
     def test_mag320ms(self):
         df = imp.mag320ms(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("imp.mag320ms returning units")
+            check_units(df)
 
     def test_mag15s(self):
         df = imp.mag15s(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("imp.mag15s returning units")
+            check_units(df)
 
     def test_mitplasma_h0(self):
         df = imp.mitplasma_h0(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("imp.mitplasma_h0 returning units")
+            check_units(df)
 
     def test_merged(self):
         df = imp.merged(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("imp.merged returning units")
+            check_units(df)
 
 
 @pytest.mark.skipif(no_pycdf, reason='Importing pycdf failed')
@@ -237,27 +282,44 @@ class TestWind:
     def test_mfi_h0(self):
         df = wind.mfi_h0(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.mfi_h0 returning units")
+            check_units(df)
 
     def test_mfi_h2(self):
         df = wind.mfi_h2(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.mfi_h2 returning units")
+            check_units(df)
 
     def test_threedp_pm(self):
         df = wind.threedp_pm(self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.threedp_pm returning units")
+            check_units(df)
 
     def test_threedp_sfpd(self):
         starttime = datetime(2002, 1, 1, 0, 0, 0)
         endtime = datetime(2002, 1, 1, 23, 59, 59)
         df = wind.threedp_sfpd(starttime, endtime)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.threedp_sfpd returning units")
+            check_units(df)
 
     def test_swe_h3(self):
         df = wind.swe_h3(self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.swe_h3 returning units")
+            check_units(df)
 
     def test_swe_h1(self):
         df = wind.swe_h1(self.starttime, self.endtime)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("wind.swe_h1 returning units")
+            check_units(df)
 
 
 @pytest.mark.skipif(no_pycdf, reason='Importing pycdf failed')
@@ -272,10 +334,16 @@ class TestMMS:
     def test_fgm_survey(self):
         df = mms.fgm_survey(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("mms.fgm_survey returning units")
+            check_units(df)
 
     def test_fpi_dis_moms(self):
         df = mms.fpi_dis_moms(self.probe, 'fast', self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("mms.fpi_dis_moms returning units")
+            check_units(df)
 
 
 @pytest.mark.data
@@ -289,7 +357,9 @@ class TestHelios:
     def test_merged(self):
         df = helios.merged(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("helios.merged returning units")
+            check_units(df)
 
         starttime = datetime(2000, 1, 1, 0, 0, 0)
         endtime = datetime(2000, 1, 2, 0, 0, 0)
@@ -299,6 +369,9 @@ class TestHelios:
     def test_corefit(self):
         df = helios.corefit(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("helios.corefit returning units")
+            check_units(df)
 
         starttime = datetime(2000, 1, 1, 0, 0, 0)
         endtime = datetime(2000, 1, 2, 0, 0, 0)
@@ -308,11 +381,16 @@ class TestHelios:
     def test_6sec_ness(self):
         df = helios.mag_ness(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("helios.mag_ness returning units")
+            check_units(df)
 
     def test_mag_4hz(self):
         df = helios.mag_4hz(self.probe, self.starttime, self.endtime)
         check_datetime_index(df)
-        check_units(df)
+        if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+            print("helios.mag_4hz returning units")
+            check_units(df)
 
 
 class TestHelper:
