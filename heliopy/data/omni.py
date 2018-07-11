205fb741c97695fc2eb622c36cc4dc1c2c5ef163
abotiamnot <yatharthrai16@ducic.ac.in>
2018-07-11 23:53:25 +0530

    OMNI now returns data with units

--- heliopy/data/omni.py
+++ heliopy/data/omni.py
@@ -7,7 +7,6 @@ https://cdaweb.gsfc.nasa.gov/pub/data/omni.
 import pathlib as path
 
 import pandas as pd
-import numpy as np
 import astropy.units as u
 import datetime as dt
 from collections import OrderedDict
@@ -43,7 +42,7 @@ def low(starttime, endtime, try_download=True):
     fnames = []
     names = ['Year', 'Decimal Day', 'Hour', 'Bartels Rotation Number',
              'ID IMF Spacecraft', 'ID SW Plasma Spacecraft',
-             '# of points (IMF Average)', '# of points (Plasma Average)',
+             'points(IMF Average)', 'points(Plasma Average)',
              '|B|', 'Magnitude of Avg Field Vector',
              'Lat. Angle of Aver. Field Vector',
              'Long. Angle of Aver. Field Vector', 'Bx GSE, GSM', 'By GSE',
@@ -59,7 +58,60 @@ def low(starttime, endtime, try_download=True):
              'Proton Flux > 30MeV',
              'Proton Flux > 60MeV', 'flag', 'ap index',
              'f10.7 index', 'PC(N) index', 'AL index (Kyoto)',
-             'AU index (Kyoto)', 'Magnetosonic Mach Number']
+             'AU index (Kyoto)', 'Magnetosonic Mach No.']
+    sfu = u.def_unit('sfu', 10**-22 * u.m**-2 * u.Hz**-1)
+    units = OrderedDict([('Bartels Rotation Number', u.dimensionless_unscaled),
+                         ('ID IMF Spacecraft', u.dimensionless_unscaled),
+                         ('ID SW Plasma Spacecraft', u.dimensionless_unscaled),
+                         ('points(IMF Average)', u.dimensionless_unscaled),
+                         ('points(Plasma Average)', u.dimensionless_unscaled),
+                         ('|B|', u.nT),
+                         ('Magnitude of Avg Field Vector', u.nT),
+                         ('Lat. Angle of Aver. Field Vector', u.deg),
+                         ('Long. Angle of Aver. Field Vector', u.deg),
+                         ('Bx GSE, GSM', u.nT),
+                         ('By GSE', u.nT),
+                         ('Bz GSE', u.nT),
+                         ('By GSM', u.nT),
+                         ('Bz GSM', u.nT),
+                         ('sigma |B|', u.nT),
+                         ('sigma B', u.nT),
+                         ('sigma Bx', u.nT),
+                         ('sigma By', u.nT),
+                         ('sigma Bz', u.nT),
+                         ('Proton Temperature', u.K),
+                         ('Proton Density', u.cm**-3),
+                         ('Plasma Flow Speed', u.km / u.s),
+                         ('Plasma Flow Long. Angle', u.deg),
+                         ('Plasma Flow Lat. Angle', u.deg),
+                         ('Na/Np', u.dimensionless_unscaled),
+                         ('Flow Pressure', u.nPa),
+                         ('sigma T', u.K),
+                         ('sigma N', u.cm**-3),
+                         ('sigma V', u.km / u.s),
+                         ('sigma phi V', u.deg),
+                         ('sigma theta V', u.deg),
+                         ('sigma Na/Np', u.dimensionless_unscaled),
+                         ('Electric Field', u.mV / u.m),
+                         ('Plasma Beta', u.K * u.cm**-3 * u.nT**-2),
+                         ('Alfven Mach Number', u.km * u.cm**-3 / u.nT / u.s),
+                         ('Kp', u.dimensionless_unscaled),
+                         ('R', u.dimensionless_unscaled),
+                         ('AE Index', u.nT),
+                         ('DST Index', u.nT),
+                         ('Proton Flux > 1MeV', u.cm**-2),
+                         ('Proton Flux > 2MeV', u.cm**-2),
+                         ('Proton Flux > 4MeV', u.cm**-2),
+                         ('Proton Flux > 10MeV', u.cm**-2),
+                         ('Proton Flux > 30MeV', u.cm**-2),
+                         ('Proton Flux > 60MeV', u.cm**-2),
+                         ('flag', u.dimensionless_unscaled),
+                         ('ap index', u.nT),
+                         ('PC(N) index', u.dimensionless_unscaled),
+                         ('AL index (Kyoto)', u.nT),
+                         ('AU index (Kyoto)', u.nT),
+                         ('Magnetosonic Mach No.', u.dimensionless_unscaled),
+                         ('f10.7 index', sfu)])
     extension = '.dat'
     for year in range(starttime.year, endtime.year + 1):
         fnames.append("omni2_{}".format(year))
@@ -81,7 +133,7 @@ def low(starttime, endtime, try_download=True):
         time_index = convert_datetime(year, day_list, hour_list, len_)
         thisdata['Time'] = pd.to_datetime(time_index)
         thisdata = thisdata.set_index('Time')
-        # thisdata = thisdata.drop(['Year', 'Decimal Day', 'Hour'])
+        thisdata = thisdata.drop(['Year', 'Decimal Day', 'Hour'], axis=1)
         return thisdata
 
     def convert_datetime(year, day_list, hour_list, len_):
@@ -94,4 +146,4 @@ def low(starttime, endtime, try_download=True):
 
     return util.process(dirs, fnames, extension, local_base_dir,
                         remote_base_url, download_func, processing_func,
-                        starttime, endtime)
+                        starttime, endtime, units=units)
