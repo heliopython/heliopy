4d872e3f7862c0b57e4c18db451656820e6b1102
abotiamnot <yatharthrai16@ducic.ac.in>
2018-07-14 22:52:35 +0530

    Converted messenger to util.process

--- heliopy/data/messenger.py
+++ heliopy/data/messenger.py
@@ -5,17 +5,17 @@ All data is publically available at ftp://spdf.gsfc.nasa.gov/pub/data/messenger
 """
 import os
 import pandas as pd
+import pathlib as path
 
 from heliopy.data import util
 from heliopy import config
 
-data_dir = config['download_dir']
-use_hdf = config['use_hdf']
-mess_dir = os.path.join(data_dir, 'messenger')
-remote_mess_dir = 'ftp://spdf.gsfc.nasa.gov/pub/data/messenger'
+data_dir = path.Path(config['download_dir'])
+mess_dir = data_dir / 'messenger'
+remote_mess_dir = 'ftp://spdf.gsfc.nasa.gov/pub/data/messenger/'
 
 
-def mag_rtn(starttime, endtime):
+def mag_rtn(starttime, endtime, try_download=True):
     """
     Import magnetic field in RTN coordinates from Messenger.
 
@@ -31,52 +31,46 @@ def mag_rtn(starttime, endtime):
     data : DataFrame
     """
     # Directory relative to main WIND data directory
-    relative_dir = 'rtn'
-
-    daylist = util._daysplitinterval(starttime, endtime)
+    relative_dir = path.Path('rtn')
+    fnames = []
+    dirs = []
+    extension = '.cdf'
+    keys = {'B_normal': 'Bn',
+            'B_radial': 'Br',
+            'B_tangential': 'Bt',
+            'Epoch': 'Time',
+            'azimuth_ecliptic': 'sc_Az',
+            'latitude_ecliptic': 'sc_Lat',
+            'radialDistance': 'sc_r',
+            'MissionElapsedTime': 'mission_time'}
     data = []
+    daylist = util._daysplitinterval(starttime, endtime)
     for day in daylist:
         date = day[0]
-
-        this_relative_dir = os.path.join(relative_dir, str(date.year))
-        hdffile = 'messenger_mag_rtn_' +\
+        this_relative_dir = relative_dir / str(date.year)
+        filename = 'messenger_mag_rtn_' +\
             str(date.year) +\
             str(date.month).zfill(2) +\
             str(date.day).zfill(2) +\
-            '_v01.hdf'
-        hdfloc = os.path.join(mess_dir, this_relative_dir, hdffile)
-        # Try to load hdf file
-        if os.path.isfile(hdfloc):
-            df = pd.read_hdf(hdfloc)
-            data.append(df)
-            continue
+            '_v01'
+        fnames.append(filename)
+        dirs.append(this_relative_dir)
 
-        filename = hdffile[:-4] + '.cdf'
-        # Absolute path to local directory for this data file
-        local_dir = os.path.join(mess_dir, this_relative_dir)
-        util._checkdir(local_dir)
+    local_base_dir = mess_dir
+    remote_base_url = remote_mess_dir
 
-        remote_url = os.path.join(remote_mess_dir, this_relative_dir)
+    def download_func(remote_base_url, local_base_dir,
+                      directory, fname, extension):
+        remote_url = remote_base_url + str(directory)
+        filename = fname + extension
+        local_dir = local_base_dir / directory
+        util._download_remote(remote_url, filename, local_dir)
 
-        cdf = util.load(filename, local_dir, remote_url, guessversion=True)
-        if cdf is None:
-            print('File {}/{} not available\n'.format(
-                remote_url, filename))
-            continue
+    def processing_func(cdf):
+        thisdata = util.cdf2df(cdf, 'Epoch', keys)
+        return thisdata
 
-        keys = {'B_normal': 'Bn',
-                'B_radial': 'Br',
-                'B_tangential': 'Bt',
-                'Epoch': 'Time',
-                'azimuth_ecliptic': 'sc_Az',
-                'latitude_ecliptic': 'sc_Lat',
-                'radialDistance': 'sc_r',
-                'MissionElapsedTime': 'mission_time'}
-        df = util.cdf2df(cdf, index_key='Epoch', keys=keys)
-
-        if use_hdf:
-            hdffile = filename[:-4] + '.hdf'
-            df.to_hdf(hdfloc, key='data', mode='w')
-        data.append(df)
-
-    return util.timefilter(data, starttime, endtime)
+    return util.process(dirs, fnames, extension, local_base_dir,
+                        remote_base_url, download_func, processing_func,
+                        starttime, endtime, keys=keys,
+                        try_download=try_download)
