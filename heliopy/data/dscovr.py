6b49415cc2a8b028be86736005e4681f2049b8b9
abotiamnot <yatharthrai16@ducic.ac.in>
2018-07-20 23:36:29 +0530

    Added DSCOVR module

---
+++ heliopy/data/dscovr.py
@@ -0,0 +1,71 @@
+"""
+Methods for importing data from the DSCOVR.
+
+All data is publically available at
+ftp://spdf.gsfc.nasa.gov/pub/data/dscovr/.
+"""
+import pathlib as path
+
+import pandas as pd
+import astropy.units as u
+import datetime as dt
+from collections import OrderedDict
+
+from heliopy.data import util
+from heliopy import config
+
+data_dir = path.Path(config['download_dir'])
+dscovr_dir = data_dir / 'dscovr'
+dscovr_url = 'https://spdf.gsfc.nasa.gov/pub/data/dscovr/'
+
+def mag_h0(starttime, endtime):
+    """
+    Import data from DSCOVR Spacecraft.
+    Parameters
+    ----------
+        starttime : datetime
+            Interval start time.
+        endtime : datetime
+            Interval end time.
+    Returns
+    -------
+        data : :class:`~sunpy.timeseries.TimeSeries`
+    """
+
+#     relative_loc = dscovr_dir / 'h0' / 'mag'
+#     remote_base_url = dscovr_url + 'h0/mag/'
+
+    dirs = []
+    fnames = []
+    extension = '.cdf'
+    daylist = util._daysplitinterval(starttime, endtime)
+    for day in daylist:
+        date = day[0]
+        filename = "dscovr_h0_mag_{}{:02}{:02}_v01".format(
+                     date.year, date.month, date.day)
+        fnames.append(filename)
+        this_relative_dir = 'h0/mag/' + str(date.year)
+        dirs.append(this_relative_dir)
+
+    local_base_dir = dscovr_dir
+    remote_base_url = dscovr_url
+
+    def download_func(remote_base_url, local_base_dir,
+                          directory, fname, extension):
+            remote_url = remote_base_url + str(directory)
+            filename = fname + extension
+            local_dir = local_base_dir / directory
+            util.load(fname + extension,
+                  local_base_dir / directory,
+                  remote_url)
+
+
+    def processing_func(cdf):
+        df = util.cdf2df(cdf, 'Epoch1')
+        df.index.name = 'Time'
+        df = df.drop(['Epoch1'], 1)
+        return df
+
+    return util.process(dirs, fnames, extension, local_base_dir,
+                        remote_base_url, download_func, processing_func,
+                        starttime, endtime)
