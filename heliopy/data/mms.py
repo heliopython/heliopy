bee0302b0fcfed5145189cbd6ce00a9e80f305f7
JeffreyBroll <jmbroll@gmail.com>
2018-10-19 09:23:22 -0400

    Update mms.py

--- heliopy/data/mms.py
+++ heliopy/data/mms.py
@@ -75,6 +75,73 @@ def fpi_dis_moms(probe, mode, starttime, endtime):
                             probe, mode, date.year, date.month, date.day, h)
             fnames.append(filename)
             dirs.append(this_relative_dir)
+            
+    remote_base_url = remote_mms_dir
+    local_base_dir = mms_dir
+
+    def download_func(remote_base_url, local_base_dir, directory,
+                      fname, remote_fname, extension):
+        remote_url = remote_base_url + str(directory)
+        util.load(fname + extension,
+                  local_base_dir / directory,
+                  remote_url)
+
+    def processing_func(cdf):
+        probestr = 'mms' + probe + '_'
+        df = util.cdf2df(cdf, 'Epoch')
+        return df
+
+    return util.process(dirs, fnames, extension, local_base_dir,
+                        remote_base_url, download_func, processing_func,
+                        starttime, endtime, units=units)
+
+def fpi_des_moms(probe, mode, starttime, endtime):
+    """
+    Import fpi ion distribution moment data.
+
+    Parameters
+    ----------
+    probe : string
+        Probe number, must be 1, 2, 3, or 4
+    mode : string
+        Data mode, must be 'fast' or 'brst'
+    starttime : datetime
+        Interval start time.
+    endtime : datetime
+        Interval end time.
+
+    Returns
+    -------
+    data : :class:`~sunpy.timeseries.TimeSeries`
+        Imported data.
+    """
+    valid_modes = ['fast', 'brst']
+    if mode not in valid_modes:
+        raise RuntimeError('Mode must be either fast or brst')
+    # Directory relative to main MMS data directory
+    relative_dir = path.Path('mms' + probe) / 'fpi' / mode / 'l2' / 'des-moms'
+    dirs = []
+    fnames = []
+    daylist = util._daysplitinterval(starttime, endtime)
+    data = []
+    units = OrderedDict([('mms{}_des_errorflags_fast'.format(probe),
+                          u.dimensionless_unscaled),
+                         ('mms{}_des_startdelphi_count_fast'.format(probe),
+                          u.dimensionless_unscaled)])
+    extension = '.cdf'
+    
+    for day in daylist:
+        date = day[0]
+        starthour = day[1].hour
+        endhour = day[2].hour + 1
+        # fips fast data product has files every two hours, so get nearest two
+        # hour stamps
+        starthour -= np.mod(starthour, 2)
+        endhour += np.mod(endhour, 2)
+        for h in range(starthour, endhour, 2):
+            this_relative_dir = (relative_dir /
+                                 str(date.year) /
+                                 str(date.month).zfill(2))
             filename = ('mms{}_fpi_{}_l2_des-moms_'
                         '{}{:02}{:02}{:02}0000_v3.3.0').format(
                             probe, mode, date.year, date.month, date.day, h)
@@ -119,14 +186,15 @@ def fgm_survey(probe, starttime, endtime):
     data : :class:`~sunpy.timeseries.TimeSeries`
         Imported data.
     """
+    
     # Directory relative to main MMS data directory
     relative_dir = path.Path('mms' + probe) / 'fgm' / 'srvy' / 'l2'
     daylist = util._daysplitinterval(starttime, endtime)
     dirs = []
     fnames = []
     #don't need so much string munging since we're asking SDC for things
-    #extension = '.cdf'
-    extension=''
+    extension = '.cdf'
+    #extension=''
     units = OrderedDict([('mms{}_fgm_mode_srvy_l2'.format(probe),
                           u.dimensionless_unscaled)])
     data = []
@@ -141,9 +209,13 @@ def fgm_survey(probe, starttime, endtime):
         # expect it to hold old file versions past their prime
         datestring = '{}-{:02}-{:02}'.format(date.year,date.month,
                                              date.day)
-        sdc_fgm_srvy = requests.get('https://lasp.colorado.edu/mms/sdc/public/files/api/v1/file_info/science?start_date='+datestring+'&end_date='+datestring+'&sc_id=mms'+probe+'&data_rate_mode=srvy&'+'instrument_id=fgm&data_level=l2')
+        sdc_req_url = 'https://lasp.colorado.edu/mms/sdc/public/files/api/v1/file_info/science?'
+        sdc_opts = 'start_date='+datestring+'&end_date='+datestring+'&sc_id=mms'+probe+'&data_rate_mode=srvy&'+'instrument_id=fgm&data_level=l2'
+        sdc_fgm_srvy = requests.get(sdc_req_url + sdc_opts)
         filename = sdc_fgm_srvy.json()['files'][0]['file_name']
 
+        filename = filename[:-4]
+        
         fnames.append(filename)
         dirs.append(this_relative_dir)
 
