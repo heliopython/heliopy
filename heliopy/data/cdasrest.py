e626ab913f4217ca62efdc7d4f4bbc5894703484
Timo Laitinen <timo.lm.laitinen@gmail.com>
2019-02-06 09:54:49 +0000

    Docstring generator for stereo (which has additional parameter for spacecraft)

--- heliopy/data/cdasrest.py
+++ heliopy/data/cdasrest.py
@@ -64,32 +64,7 @@ def _docstring_stereo(identifier, letter, description):
                description=description)
     return ds
 
-def _docstring_stereo(identifier, letter, description):
-    ds = r"""
-    {description} data.
 
-    See https://cdaweb.sci.gsfc.nasa.gov/misc/Notes{letter}.html#{identifier}
-    for more information.
-
-    Parameters
-    ----------
-    starttime : datetime
-        Interval start time.
-    endtime : datetime
-        Interval end time.
-    spacecraft: string
-        Spacecraft identifier, 'sta' or 'stb'
-
-    Returns
-    -------
-    data : :class:`~sunpy.timeseries.TimeSeries`
-    """.format(identifier=identifier,
-               letter=letter,
-               description=description)
-    return ds
-
-
-<<<<<<< HEAD
 def _day_intervals(starttime, endtime):
     interval = stime.TimeRange(starttime, endtime)
     daylist = interval.get_dates()
