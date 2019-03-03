dfbc7c5906c76152d66133c390704d63e63319e2
David Stansby <dstansby@gmail.com>
2019-03-03 16:19:59 +0000

    Add request for bug reporting when units are missing

--- heliopy/data/util.py
+++ heliopy/data/util.py
@@ -258,12 +258,16 @@ def units_attach(data, units, warn_missing_units=True):
     out : :class:`~sunpy.timeseries.TimeSeries`
         DataFrame converted into TimeSeries with units attached.
     """
+    missing_msg = ('If you are trying to auomatically download data '
+                   'with HelioPy this is a bug, please report it at '
+                   'https://github.com/heliopython/heliopy/issues')
     unit_key = list(units.keys())
     for column_name in data.columns:
         if column_name not in unit_key:
             units[column_name] = u.dimensionless_unscaled
             if warn_missing_units:
-                message = "{} column has missing units.".format(column_name)
+                message = (f"{column_name} column has missing units."
+                           f"\n{missing_msg}")
                 warnings.warn(message, Warning)
     with warnings.catch_warnings():
         warnings.simplefilter(
