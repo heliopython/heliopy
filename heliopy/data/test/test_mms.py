c77a06609f6a6b64f83ebc85ca981645706fc203
Romain Dupuis <romain.dupuis@kuleuven.be>
2019-06-04 10:37:09 +0200

    Correction to fit E302 expectation from PEP8

--- heliopy/data/test/test_mms.py
+++ heliopy/data/test/test_mms.py
@@ -12,25 +12,30 @@ starttime = datetime(2016, 1, 2, 0, 0, 0)
 endtime = datetime(2016, 1, 2, 1, 0, 0)
 probes = ['1', '4']
 
+
 @pytest.mark.parametrize("probe", probes)
 def test_fgm(probe):
     df = mms.fgm(probe, 'srvy', starttime, endtime)
     check_data_output(df)
+
 
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_dis_moms(probe):
     df = mms.fpi_dis_moms(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
+
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_des_moms(probe):
     df = mms.fpi_des_moms(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
+
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_dis_dist(probe):
     df = mms.fpi_dis_dist(probe, 'fast', starttime, endtime)
     check_data_output(df)
+
 
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_des_dist(probe):
