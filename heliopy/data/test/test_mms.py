dfa66f5333b5f599f7302d6aa918752f2e005048
Romain Dupuis <romain.dupuis@kuleuven.be>
2019-06-04 10:08:33 +0200

    Change allowed_probes of mms.py and add parameter to test_mms to check probe '4'

--- heliopy/data/test/test_mms.py
+++ heliopy/data/test/test_mms.py
@@ -3,35 +3,36 @@ import pytest
 
 from .util import check_data_output
 
+
 mms = pytest.importorskip('heliopy.data.mms')
 pytest.mark.data()
 
 
 starttime = datetime(2016, 1, 2, 0, 0, 0)
 endtime = datetime(2016, 1, 2, 1, 0, 0)
-probe = '1'
+probes = ['1', '4']
 
-
-def test_fgm():
+@pytest.mark.parametrize("probe", probes)
+def test_fgm(probe):
     df = mms.fgm(probe, 'srvy', starttime, endtime)
     check_data_output(df)
 
-
-def test_fpi_dis_moms():
+@pytest.mark.parametrize("probe", probes)
+def test_fpi_dis_moms(probe):
     df = mms.fpi_dis_moms(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
-
-def test_fpi_des_moms():
+@pytest.mark.parametrize("probe", probes)
+def test_fpi_des_moms(probe):
     df = mms.fpi_des_moms(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
-
-def test_fpi_dis_dist():
+@pytest.mark.parametrize("probe", probes)
+def test_fpi_dis_dist(probe):
     df = mms.fpi_dis_dist(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
-
-def test_fpi_des_dist():
+@pytest.mark.parametrize("probe", probes)
+def test_fpi_des_dist(probe):
     df = mms.fpi_des_dist(probe, 'fast', starttime, endtime)
     check_data_output(df)
