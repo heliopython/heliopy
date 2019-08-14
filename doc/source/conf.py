da96103ee415ccc625c3d1294952df86499c00ea
David Stansby <dstansby@gmail.com>
2019-08-14 17:33:13 +0100

    Remove old comments in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -366,18 +366,3 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-# Documents to append as an appendix to all manuals.
-#
-# texinfo_appendices = []
-
-# If false, no module index is generated.
-#
-# texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#
-# texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#
-# texinfo_no_detailmenu = False
