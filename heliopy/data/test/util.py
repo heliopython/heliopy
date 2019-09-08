Fixed some issues with omni data download, fixed output units (now as an OrderedDict) and check by astropy, fixed unit tests for MMS, Cluster and Omni, and other minor issues.

heliopy/data/test/util.py
@@ -1,29 +1,31 @@
 import astropy.units as u
-import pandas as pd
-import sunpy
+import numpy as np
+import warnings
 
 
-def check_data_output(df):
+def check_data_output(data):
     '''
     Function to check that the output of a data fetch method.
     '''
-    check_units(df)
-    if type(df) is sunpy.timeseries.timeseriesbase.GenericTimeSeries:
-        df = df.data
-    check_datetime_index(df)
-    assert df.shape[0] > 0
-    assert df.shape[1] > 0
+    check_units(data)
+    check_datetime_index(data)
+    assert len(data.time) > 0 
+    assert len(data.data_vars) > 0
+    for var in data.data_vars:
+        assert data[str(var)].shape[0] > 0
+
+        
+
+def check_datetime_index(data):
+    'Helper function to check all dataframes have a datetime index'
+    for timetag in data.time.values:
+        assert isinstance(timetag, np.datetime64)
 
 
-def check_datetime_index(df):
-    'Helper funciton to check all dataframes have a datetime index'
-    assert type(df.index[0]) == pd.Timestamp
-
-
-def check_units(df):
-    if type(df) is not sunpy.timeseries.timeseriesbase.GenericTimeSeries:
+def check_units(data):
+    if not data.attrs['Units']:
         warnings.warn("Function has no units attached", RuntimeWarning)
-        assert type(df.index[0]) == pd.Timestamp
     else:
-        for column in df.data.columns:
-            assert type(df.quantity(column)) == u.quantity.Quantity
+        for unit in data.attrs['Units'].values():
+            assert isinstance(unit,(u.Unit,u.UnrecognizedUnit,
+                                    u.IrreducibleUnit,u.CompositeUnit))