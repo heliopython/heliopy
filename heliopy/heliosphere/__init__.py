4cf794b7ba6857f55b73f70e58a14b1fc96ce168
David Stansby <dstansby@gmail.com>
2019-09-17 13:20:37 +0100

    Add Parker Spiral class

---
+++ heliopy/heliosphere/__init__.py
@@ -0,0 +1,46 @@
+import astropy.units as u
+
+
+class ParkerSpiral:
+    r"""
+    A Parker spiral mangetic field line.
+
+    Parameters
+    ----------
+    v : ~astropy.units.Quantity
+        Solar wind speed.
+    r0 : ~astropy.units.Quantity
+        Radial distance from the solar center of a reference point on
+        the spiral.
+    l0 : ~astropy.units.Quantity
+        Longitude of the reference point on the spiral.
+
+    Notes
+    -----
+    The longitude of the spiral as a funciton of r is given by
+
+    .. math::
+
+        \phi = \phi_{0} - \Omega_{\odot} \left ( r - r_{0} \right ) / v_{sw}
+
+    where $\phi_{0}$ and $r_{0}$ are a reference longitude and radial distance
+    that the spiral passes through, $\Omega_{\odot}$ is the solar rotation
+    rate, $v_{sw}$ is the solar wind speed, and $r$ is the radial distance from
+    the solar center.
+    """
+    def __init__(self, v, r0, l0):
+        self.v = v
+        self.r0 = r0
+        self.l0 = l0
+
+    def longitude(self, rs):
+        """
+        Sample the spiral longitude at given radial distances.
+
+        Parameters
+        ----------
+        rs : ~astropy.units.Quantity
+            Radial distance(s).
+        """
+        omega_sun = 14.713 * (u.deg / u.day)
+        return (self.l0 - (omega_sun * (rs - self.r0) / self.vsw)).to(u.deg)
