326c266a3df75a1eb07d9ee2541049d9f021a6f5
Matthew R. Argall <matthew.argall@unh.edu>
2019-11-20 00:18:12 -0500

    Removed old, duplicate methods. Fixes for commit checkers.

--- heliopy/data/mms.py
+++ heliopy/data/mms.py
@@ -11,7 +11,7 @@ import glob
 import datetime as dt
 import requests
 import tqdm.auto as tqdm
-from urllib
+import urllib
 
 import heliopy
 from heliopy.data import util
@@ -1665,19 +1665,19 @@ def filter_version(files, latest=None, version=None, min_version=None):
     return filtered_files
 
 
-def parse_file_names(fnames):
+def parse_file_name(fname):
     """
-    Parse file name(s) compliant with MMS file name format guidelines.
+    Parse a file name compliant with MMS file name format guidelines.
 
     Parameters
     ----------
-    fname : str, list
-        File names to be parsed.
+    fname : str
+        File name to be parsed.
 
     Returns
     -------
-    parts : list
-        A list of tuples. The tuple elements are:
+    parts : tuple
+        The tuple elements are:
             [0]: Spacecraft IDs
             [1]: Instrument IDs
             [2]: Data rate modes
@@ -1947,120 +1947,6 @@ def available_files(probe, instrument, starttime, endtime, data_rate='',
     files = r.text.split(',')
     files = filter_time(files, starttime, endtime)
     return files
-
-
-def filter_time(fnames, starttime, endtime):
-    """
-    Filter files by their start times.
-
-    Parameters
-    ----------
-    fnames : str or list
-        File names to be filtered.
-    starttime : ~datetime.datetime
-        Start date of time interval
-    endtime : ~datetime.datetime
-        End date of time interval
-
-    Returns
-    -------
-        paths : list
-            Path to the data file.
-    """
-
-    # Output
-    files = fnames
-    if isinstance(files, str):
-        files = [files]
-
-    # Parse the time out of the file name
-    parts = parse_filename(fnames)
-    fstart = [datetime.strptime(name[-2], '%Y%m%d') if len(name[-2]) == 8 else
-              datetime.strptime(name[-2], '%Y%m%d%H%M%S')
-              for name in parts]
-
-    # Sort the files by start time
-    isort = sorted(range(len(fstart)), key=lambda k: fstart[k])
-    fstart = [fstart[i] for i in isort]
-    files = [files[i] for i in isort]
-
-    # End time
-    #   - Any files that start on or before END_DATE can be kept
-    idx = [i for i, t in enumerate(fstart) if t <= endtime]
-    if len(idx) > 0:
-        fstart = [fstart[i] for i in idx]
-        files = [files[i] for i in idx]
-    else:
-        fstart = []
-        files = []
-
-    # Start time
-    #   - Any file with TSTART <= START_DATE can potentially have data
-    #     in our time interval of interest.
-    #   - Assume the start time of one file marks the end time of the
-    #     previous file.
-    #   - With this, we look for the file that begins just prior to
-    #     START_DATE and throw away any files that start before it.
-    idx = [i for i, t in enumerate(fstart) if t >= starttime]
-
-    if (len(idx) == 0) & (fstart[-1].date() == starttime.date()):
-        idx = [len(fstart) - 1]
-    elif (len(idx) != 0) & ((idx[0] != 0) & (fstart[idx[0]] != starttime)):
-        idx.insert(0, idx[0] - 1)
-
-    if len(idx) > 0:
-        fstart = [fstart[i] for i in idx]
-        files = [files[i] for i in idx]
-    else:
-        fstart = []
-        files = []
-
-    return files
-
-
-def parse_filename(fnames):
-    """
-    Construct a file name compliant with MMS file name format guidelines.
-
-    Parameters
-    ----------
-    fname : str or list
-        File names to be parsed.
-
-    Returns
-    -------
-    parts : list
-        A list of tuples. The tuple elements are:
-            [0]: Spacecraft IDs
-            [1]: Instrument IDs
-            [2]: Data rate modes
-            [3]: Data levels
-            [4]: Optional descriptor (empty string if not present)
-            [5]: Start times
-            [6]: File version number
-    """
-
-    # Allocate space
-    out = []
-
-    if type(fnames) is str:
-        files = [fnames]
-    else:
-        files = fnames
-
-    # Parse each file
-    for file in files:
-        # Parse the file names
-        parts = os.path.basename(file).split('_')
-
-        if len(parts) == 6:
-            optdesc = ''
-        else:
-            optdesc = parts[4]
-
-        out.append((*parts[0:4], optdesc, parts[-2], parts[-1][1:-4]))
-
-    return out
 
 
 def download_files(probe, instrument, data_rate, starttime, endtime,
