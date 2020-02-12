ce9bf925f5d29981763c84aa084382ead7c34a8e
tlml <tlml@users.noreply.github.com>
2020-02-12 15:55:32 +0000

    STEREO SEPT level 2 data implemented, checks to be done

--- heliopy/data/stereo.py
+++ heliopy/data/stereo.py
@@ -11,9 +11,10 @@ Please refer to the documentation of the data found there.
 from collections import OrderedDict
 import pathlib
 from datetime import datetime, timedelta
-from dateutil.rrule import rrule, MONTHLY
+from dateutil.rrule import rrule, MONTHLY, DAILY
 
 import astropy.units as u
+import astropy.time
 import pandas as pd
 
 from heliopy import config
@@ -135,6 +136,7 @@ def ste_l1(starttime, endtime, spacecraft):
 ste_l1.__doc__ = _docstring('STA_L1_STE',
                             'STEREO IMPACT/STE Level 1 Data')
 
+# +
 def het_l1(starttime, endtime, spacecraft, timeres):
     """
     Import data from STEREO HET web Interface.
@@ -305,3 +307,152 @@ def het_l1(starttime, endtime, spacecraft, timeres):
                         remote_base_url, download_func, processing_func,
                         starttime, endtime, units=units)
 
+
+def sept_l2(starttime, endtime, spacecraft, species, view, timeres):
+    """
+    Import STEREO SEPT level 2 data.
+
+    Data documentation is available at
+    
+    http://www2.physik.uni-kiel.de/stereo/index.php?doc=data
+    
+    Please note that the Time corresponds to the centre of the time bin.
+
+    Parameters
+    ----------
+        starttime : datetime
+            Interval start time.
+        endtime : datetime
+            Interval end time.
+        spacecraft: string
+            Either "sta" or "stb"
+        species: string
+            Either "e" or "i", for electrons and ions
+        view: string
+            viewing direction: "sun", "asun", "north", "south" or "omni"
+        timeres: string
+            Valid values are "1m", "10m", "1h", "1d"
+            
+    Returns
+    -------
+        data : :class:`~sunpy.timeseries.TimeSeries`
+    """
+    
+    sept_l2_url = 'http://www2.physik.uni-kiel.de/stereo/data/sept/level2'
+    
+    data_dir = pathlib.Path(config['download_dir'])
+    
+    stereo_sept_l2_dir = data_dir / 'stereo'
+
+    if timeres not in ("1m", "10m", "1h", "1d"):
+        raise ValueError("Invalid time resolution, should be 1m, 10m, 1h or 1d")
+
+    timedirs = {"1m": "1min",
+              "10m": "10min",
+              "1h": "1h", 
+              "1d": "1d"}
+    
+    sc_identifiers = {"STA": "ahead",
+                        "STB": "behind"}
+    
+    identifier = _identifier_select(spacecraft)
+    sc_ident = sc_identifiers[identifier]
+    speciesident = "ele" if species=="e" else "ion"
+        
+    local_base_dir = stereo_sept_l2_dir / identifier / (identifier+"_L2_SEPT")
+    remote_base_url = sept_l2_url + "/" 
+
+    if species == 'e':
+        names = ['Julian', 'Year', 'Doy',
+                'hour', 'minute', 'second',
+                'e_45_55', 'e_55_65', 'e_65_75',
+                'e_75_85', 'e_85_105', 'e_105_125',
+                'e_125_145', 'e_145_165', 'e_165_195',
+                'e_195_225', 'e_225_255', 'e_255_295',
+                'e_295_335', 'e_335_375', 'e_375_425',
+                'e_45_55_unc', 'e_55_65_unc', 'e_65_75_unc',
+                'e_75_85_unc', 'e_85_105_unc', 'e_105_125_unc',
+                'e_125_145_unc', 'e_145_165_unc', 'e_165_195_unc',
+                'e_195_225_unc', 'e_225_255_unc', 'e_255_295_unc',
+                'e_295_335_unc', 'e_335_375_unc', 'e_375_425_unc',
+                'dt']
+        units = OrderedDict()
+        for n in names[:6]:
+            units.update({n:u.dimensionless_unscaled})
+        for n in names[6:21]:
+            units.update({n:(u.cm**2*u.s*u.sr*u.MeV)**-1})
+        for n in names[21:-1]:
+            units.update({n:u.dimensionless_unscaled})
+        units.update({names[-1]:u.s})
+    elif species == 'i':
+        names = ['Julian', 'Year', 'Doy',
+                'hour', 'minute', 'second',
+                'i_84_92', 'i_92_101', 'i_101_110',
+                'i_110_118', 'i_118_137', 'i_137_155',
+                'i_155_174', 'i_174_192', 'i_192_219',
+                'i_219_246', 'i_246_273', 'i_273_312',
+                'i_312_350', 'i_350_389', 'i_389_438',
+                'i_438_496', 'i_496_554', 'i_554_622',
+                'i_622_700', 'i_700_788', 'i_788_875',
+                'i_875_982', 'i_982_1111', 'i_1111_1250',
+                'i_1250_1399', 'i_1399_1578', 'i_1578_1767',
+                'i_1767_1985', 'i_1985_2223', 'i_2223_6500',
+                'i_84_92_unc', 'i_92_101_unc', 'i_101_110_unc',
+                'i_110_118_unc', 'i_118_137_unc', 'i_137_155_unc',
+                'i_155_174_unc', 'i_174_192_unc', 'i_192_219_unc',
+                'i_219_246_unc', 'i_246_273_unc', 'i_273_312_unc',
+                'i_312_350_unc', 'i_350_389_unc', 'i_389_438_unc',
+                'i_438_496_unc', 'i_496_554_unc', 'i_554_622_unc',
+                'i_622_700_unc', 'i_700_788_unc', 'i_788_875_unc',
+                'i_875_982_unc', 'i_982_1111_unc', 'i_1111_1250_unc',
+                'i_1250_1399_unc', 'i_1399_1578_unc', 'i_1578_1767_unc',
+                'i_1767_1985_unc', 'i_1985_2223_unc', 'i_2223_6500_unc',
+                'dt']
+        units = OrderedDict()
+        for n in names[:6]:
+            units.update({n:u.dimensionless_unscaled})
+        for n in names[6:36]:
+            units.update({n:(u.cm**2*u.s*u.sr*u.MeV)**-1})
+        for n in names[36:-1]:
+            units.update({n:u.dimensionless_unscaled})
+        units.update({names[-1]:u.s})        
+    else:
+        raise ValueError("Invalid species, should be 'e' or 'i'")
+        
+    extension = ".dat"
+    # Create monthly filenames
+    sttime = datetime(starttime.year, starttime.month, starttime.day)
+    fnames = ["sept_"+sc_ident+"_"+speciesident+"_"+view+"_"+dt.strftime("%Y")+
+              "_"+dt.strftime("%j")+"_"+timedirs[timeres]+"_l2_v03" 
+              for dt in rrule(DAILY, dtstart=sttime, until=endtime)]
+    
+    dirs = [pathlib.Path(sc_ident) / timedirs[timeres] / dt.strftime("%Y") 
+            for dt in rrule(DAILY, dtstart=sttime, until=endtime)]
+    
+    def download_func(remote_base_url, local_base_dir,
+                      directory, fname, remote_fname, extension):
+        url = f"{remote_base_url}/{directory}"
+        util._download_remote(url,
+                              fname + extension,
+                              local_base_dir / directory)
+    
+    def processing_func(file):
+
+        # Find end of header
+        lines = file.readlines()
+        endline = [i for i,l in enumerate(lines) if l=="#BEGIN DATA\n"][0]
+        file.seek(0)
+        
+        thisdata = pd.read_table(file, names=names, delim_whitespace=True, header=endline)
+        jdt = astropy.time.Time(thisdata.Julian, format="jd")
+        thisdata['Time'] = jdt.to_datetime()
+
+        thisdata = thisdata.set_index('Time')
+#         thisdata = thisdata.drop(["Verse", "Year0", "Month0", "day0", "hour0minute0"], axis=1)
+#         if timeres != "1m":
+#             thisdata = thisdata.drop(["Year1", "Month1", "day1", "hour1minute1"], axis=1)
+        return thisdata
+
+    return util.process(dirs, fnames, extension, local_base_dir,
+                        remote_base_url, download_func, processing_func,
+                        starttime, endtime, units=units)
