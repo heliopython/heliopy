47a7bc2c8be04f24c5d16f4fb9a48455dc0f720e
David Stansby <dstansby@gmail.com>
2020-03-07 18:02:13 +0000

    Skip MMS tests

--- heliopy/data/test/test_mms.py
+++ heliopy/data/test/test_mms.py
@@ -13,6 +13,7 @@ endtime = datetime(2016, 1, 2, 1, 0, 0)
 probes = ['1', '4']
 
 
+@pytest.mark.skip()
 @pytest.mark.parametrize("probe", probes)
 def test_fgm(probe):
     df = mms.fgm(probe, 'srvy', starttime, endtime)
@@ -32,18 +33,18 @@ def test_fpi_des_moms(probe):
 
 
 # These tests take a *long* time to run, so comment them out
-"""
+@pytest.mark.skip()
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_dis_dist(probe):
     df = mms.fpi_dis_dist(probe, 'fast', starttime, endtime)
     check_data_output(df)
 
 
+@pytest.mark.skip()
 @pytest.mark.parametrize("probe", probes)
 def test_fpi_des_dist(probe):
     df = mms.fpi_des_dist(probe, 'fast', starttime, endtime)
     check_data_output(df)
-"""
 
 
 def test_narrow_interval():
