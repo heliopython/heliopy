4202075f52f260404ddb99b1f93a44360dcbf9e3
Richard Haythornthwaite <45592645+RichardHaythorn@users.noreply.github.com>
2020-04-08 17:35:41 +0100

    Tidying accidental commits

--- heliopy/data/util.py
+++ heliopy/data/util.py
@@ -1025,16 +1025,13 @@ def _download_remote(remote_url, filename, local_dir):
     dl_path = path.Path(local_dir) / filename
     remote_url = _fix_url(remote_url)
     remote_url = remote_url + '/' + filename
-    
     with requests.head(remote_url,allow_redirects=True) as r:
         if r.status_code != requests.codes.ok:
-            raise NoDataError
-            
+            raise NoDataError           
     print(f'Downloading {remote_url} to {dl_path}')
     fname, _ = urlreq.urlretrieve(remote_url,
                                   filename=str(dl_path),
                                   reporthook=_reporthook)
-    print(fname)
     print('\n')
 
 
