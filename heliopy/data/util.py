b7d2cdfe330662f854947abc16d57008d9690b34
David Stansby <dstansby@gmail.com>
2020-06-06 18:55:40 +0100

    Improve warning for missing units

--- heliopy/data/util.py
+++ heliopy/data/util.py
@@ -492,13 +492,17 @@ def units_attach(data, units, warn_missing_units=True):
                    'with HelioPy this is a bug, please report it at '
                    'https://github.com/heliopython/heliopy/issues')
     unit_keys = list(units.keys())
+    missing_names = []
     for column_name in data.columns:
         if column_name not in unit_keys:
             units[column_name] = u.dimensionless_unscaled
-            if warn_missing_units:
-                message = (f"{column_name} column has missing units."
-                           f"\n{missing_msg}")
-                warnings.warn(message, Warning)
+            missing_names.append(column_name)
+
+    if warn_missing_units:
+        missing_names = '[' + ", ".join(missing_names) + ']'
+        message = (f"{missing_names} columns have missing units."
+                   f"\n{missing_msg}")
+        warnings.warn(message, Warning)
 
     timeseries_data = ts.GenericTimeSeries(data, units=units)
     return timeseries_data
