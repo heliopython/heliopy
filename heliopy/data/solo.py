0045cbaeb638ed71d27a5bc68e45c32633e63d9c
David Stansby <dstansby@gmail.com>
2020-08-16 20:09:56 +0100

    Clean up code

--- heliopy/data/solo.py
+++ heliopy/data/solo.py
@@ -18,18 +18,21 @@ class _SoloDownloader(util.Downloader):
         """
         Parameters
         ----------
-        level : str
-            One of ``['TELEMETRY', 'PLANNING', 'LOW_LATENCY']``.
         """
+        helper._check_in_list(['LL02'], level=level)
         self.level = level
+        if self.level[:2] == 'LL':
+            self.product_type = 'LOW_LATENCY'
+
+        helper._check_in_list(['MAG'], instrument=instrument)
         self.instrument = instrument
 
     def intervals(self, starttime, endtime):
-        base_url = 'http://soar.esac.esa.int/soar-sl-tap/tap/sync?REQUEST=doQuery&'
+        base_url = ('http://soar.esac.esa.int/soar-sl-tap/tap/'
+                    'sync?REQUEST=doQuery&')
         begin_time = time.parse_time(starttime).isot.replace('T', '+')
         end_time = time.parse_time(endtime).isot.replace('T', '+')
         # Need to manually set the intervals based on a query
-        # QUERY=SELECT+*+FROM+v_data_item+WHERE+instrument=%27MAG%27+AND+level=%27LL02%27+AND+begin_time%3E=%272020-08-02+00:00:00.000%27+AND+end_time%3C=%272020-08-04+00:00:00.000%27
         request_dict = {}
         request_dict['LANG'] = 'ADQL'
         request_dict['FORMAT'] = 'json'
@@ -37,8 +40,12 @@ class _SoloDownloader(util.Downloader):
         query = {}
         query['SELECT'] = '*'
         query['FROM'] = 'v_data_item'
-        query['WHERE'] = f"instrument='{self.instrument}'+AND+level='{self.level}'+AND+begin_time<='{end_time}'+AND+end_time>='{begin_time}'"
-        request_dict['QUERY'] = '+'.join([f'{item}+{query[item]}' for item in query])
+        query['WHERE'] = (f"instrument='{self.instrument}'+AND+"
+                          f"level='{self.level}'+AND+"
+                          f"begin_time<='{end_time}'+AND+"
+                          f"end_time>='{begin_time}'")
+        request_dict['QUERY'] = '+'.join([f'{item}+{query[item]}' for
+                                          item in query])
 
         request_str = ''
         request_str = [f'{item}={request_dict[item]}' for item in request_dict]
@@ -59,7 +66,8 @@ class _SoloDownloader(util.Downloader):
         intervals = []
         for start, end in zip(info['begin_time'], info['end_time']):
             intervals.append(time.TimeRange(start, end))
-        self.file_ids = {interval.start.isot: id for interval, id in zip(intervals, info['data_item_id'])}
+        self.file_ids = {interval.start.isot: id for interval, id in
+                         zip(intervals, info['data_item_id'])}
         # TODO: log the number of intervals found here
         return intervals
 
@@ -67,7 +75,9 @@ class _SoloDownloader(util.Downloader):
         return self.file_ids[interval.start.isot]
 
     def download(self, interval):
-        base_url = 'http://soar.esac.esa.int/soar-sl-tap/data?retrieval_type=PRODUCT&product_type=LOW_LATENCY&data_item_id='
+        base_url = ('http://soar.esac.esa.int/soar-sl-tap/data?'
+                    f'retrieval_type=PRODUCT&product_type={self.product_type}&'
+                    'data_item_id=')
         url = base_url + self._file_id(interval)
         try:
             util._download_url(url, self.local_path(interval))
@@ -81,18 +91,22 @@ class _SoloDownloader(util.Downloader):
 
     def local_dir(self, interval):
         # TODO: work out how to be more granular than just solar orbiter
-        return pathlib.Path('solar_orbiter')
+        return pathlib.Path('solar_orbiter') / self.instrument / self.level
 
     def fname(self, interval):
         return f'{self._file_id(interval)}.cdf'
 
 
 def download(starttime, endtime, instrument, level):
+    """
+    starttime
+    endtime
+    instrument : str
+        One of ``['MAG']``.
+    level : str
+        One of ``['LL02']``.
+    """
     instrument = instrument.upper()
-    helper._check_in_list(['MAG'], instrument=instrument)
-
     level = level.upper()
-    helper._check_in_list(['LL02'], level=level)
-
     dl = _SoloDownloader(instrument, level)
     return dl.load(starttime, endtime)
