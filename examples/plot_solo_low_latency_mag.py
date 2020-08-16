2cd63e52d7ac54da0f206f2afc0089b598c66c88
David Stansby <dstansby@gmail.com>
2020-08-16 21:17:32 +0100

    Add example

---
+++ examples/plot_solo_low_latency_mag.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import warnings
+
+import astropy.units as u
+import matplotlib.pyplot as plt
+from matplotlib import dates as mdates
+import numpy as np
+
+from heliopy.data.solo import download
+
+# TODO: data file on the 3rd isn't working
+data = download(datetime(2020, 7, 10), datetime(2020, 8, 20), 'MAG', 'LL02')
+print(data.columns)
+
+modB = np.sqrt(data.quantity('B_RTN_0')**2 +
+               data.quantity('B_RTN_1')**2 +
+               data.quantity('B_RTN_2')**2)
+data = data.add_column('modB', modB)
+
+fig, axs = plt.subplots(nrows=2, sharex=True)
+
+ax = axs[0]
+ax.plot(data.index, data.quantity('modB'))
+ax.set_ylabel('nT')
+ax.set_title(r'$|B|$')
+ax.set_ylim(bottom=0)
+
+ax = axs[1]
+ax.plot(data.index, data.quantity('B_RTN_0'))
+ax.set_ylabel('nT')
+ax.set_title(r'$B_{r}$')
+ax.axhline(0, color='black', linewidth=1, linestyle='--')
+
+fig.suptitle('Solar Orbiter MAG low latency (not for science use)')
+ax.xaxis.set_major_formatter(
+    mdates.ConciseDateFormatter(mdates.AutoDateLocator()))
+plt.show()
