0fcf126613897cc1bb6e74deb4bc6478ca52d353
David Stansby <dstansby@gmail.com>
2022-01-25 16:10:19 +0000

    Add import errors

--- heliopy/spice/__init__.py
+++ heliopy/spice/__init__.py
@@ -1,27 +1,3 @@
-"""
-Overview
---------
-`SPICE`_ is a NASA toolkit for calculating the position of bodies
-(including planets and spacecraft) within the solar system. This module builds
-on the `spiceypy`_ package to provide a high level interface to the SPICE
-toolkit for performing orbital calculations using spice kernels.
+from heliopy import raise_import_error
 
-Integration with :mod:`astropy.coordinates`
--------------------------------------------
-As well as allowing positions to be calculated in any coordinate system defined
-in the `SPICE`_ toolkit, :mod:`heliopy.spice` can also construct
-:class:`astropy.coordinates.SkyCoord` objects if the frame is implemented in
-astropy or SunPy. See the documentaiton of
-:meth:`heliopy.spice.Trajectory.coords()` for information on which frames are
-supported.
-
-.. _SPICE: https://naif.jpl.nasa.gov/naif/toolkit.html
-.. _spiceypy: https://spiceypy.readthedocs.io/en/master/
-"""
-import heliopy.data.spice as dataspice
-from .spice import *
-
-
-for kernel in dataspice.generic_kernels:
-    k = dataspice.get_kernel(kernel.short_name)
-    furnish(k)
+raise_import_error()
---
+++ heliopy/util/__init__.py
@@ -0,0 +1,3 @@
+from heliopy import raise_import_error
+
+raise_import_error()
